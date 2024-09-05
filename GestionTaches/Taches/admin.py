f84980c455a4e30a0ff42ae58f078c95eceae4c7
jordanlavenant <jordan.lavenant@laposte.net>
2024-09-05 10:52:37 +0200

    Migrations & models tasks

--- GestionTaches/Taches/admin.py
+++ GestionTaches/Taches/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+  list_display = ('name', 'description', 'created_date', 'closed', 'due_date', 'schedule_date')
+  read_only = ('created_date')
+
+admin.site.register(Task, TaskAdmin)
--- GestionTaches/Taches/models.py
+++ GestionTaches/Taches/models.py
@@ -1,11 +1,27 @@
 from django.db import models
+from datetime import datetime, timedelta, date
+from django.utils.html import format_html
 
 class Task(models.Model):
-  name = models.CharField(max_length = 250)
+  name = models.CharField(max_length=250)
   description = models.TextField()
-  created_date = models.DateField(auto_now_add = True)
-  closed = models.BooleanField(default = False)
-
+  created_date = models.DateField(auto_now_add=True)
+  closed = models.BooleanField(default=False)
+  due_date = models.DateField(null=True)
+  schedule_date = models.DateField(default=datetime.now()+timedelta(days=7))
 
   def __str__(self):
     return self.name
+  
+  # def colored_due_date(self):
+  #   due_date = django_date(self.due_date,"d F Y")
+  #   if self.due_date is None or self.due_date-timedelta(days=7) > date.today():
+  #     color = "green"
+  #   elif self.due_date < date.today():
+  #     color = "red"
+  #   else:
+  #     color = "orange"
+  #   return format_html("<span style=color:%s>%s</span>" % (color, due_date))
+
+  
+  # colored_due_date.allow_tags = True
